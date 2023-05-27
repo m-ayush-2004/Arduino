d16c434b3ea01b7cd8ce96b65994abd83c3f9506
Ayush <69383891+m-ayush-2004@users.noreply.github.com>
2023-05-27 18:41:34 +0530

    Update color_sensor_TCS3200.ino

--- color_sensor_TCS3200/color_sensor_TCS3200.ino
+++ color_sensor_TCS3200/color_sensor_TCS3200.ino
@@ -1,3 +1,33 @@
+/*
+materials
+
+ARDUINO uno,
+TCS-3200,
+jumper wires
+Bread boarD
+
+
+
+connections to TCS-3200:
+
+  ----------------------
+ |   UNO      TCS-3200 |
+  ----------------------
+ |   S0   -->     R    |
+  ----------------------
+ |   S1   -->     G    |
+  ----------------------
+ |   S2   -->     B    |
+  ----------------------
+ |   S3   -->    RX    |
+  ----------------------
+ |  OUT   -->    TX    |
+  ----------------------
+ |  GND   -->   GND    |
+  ---------------------- 
+ | +5.0V  -->    VCC   |
+  ----------------------
+*/
 #define s0 2
 #define s1 3
 #define s2 4
@@ -15,8 +45,6 @@ void setup() {
   digitalWrite(s1,HIGH);
   pinMode(output,INPUT);
   Serial.begin(9600);
-  
-  // put your setup code here, to run once:
 }
 
 void loop() {
@@ -26,7 +54,9 @@ void loop() {
   delay(500);
   B= blue_data();
   delay(500);
-  
+//   printing the values to the serial in R,G,B order 
+//   this order is used in python serial at COM'N'
+//   we use these values to generate the output matplotlib graphs
   Serial.print(R);
   Serial.print(",");
   Serial.print(G);
@@ -37,7 +67,7 @@ void loop() {
 int red_data(){
   digitalWrite(s2,LOW);
   digitalWrite(s3,LOW);
-  //red
+  //red input pulse read
   int data=pulseIn(output,LOW);
   return data;
 }
@@ -45,7 +75,7 @@ int red_data(){
 int green_data(){
   digitalWrite(s2,HIGH);
   digitalWrite(s3,HIGH);
-  //green
+  //green input pulse read
   int data=pulseIn(output,LOW);
   return data;
 }
@@ -53,7 +83,7 @@ int green_data(){
 int blue_data(){
   digitalWrite(s2,LOW);
   digitalWrite(s3,HIGH);
-  //blue
+  //blue input pulse read
   int data=pulseIn(output,LOW);
   return data;
 }
